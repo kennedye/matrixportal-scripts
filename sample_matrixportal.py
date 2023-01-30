3ce8e0da0a63cbc975aff7d684b90f613f898d1f
Erik Kennedy <kennedye@users.noreply.github.com>
2023-01-29 20:48:44 -0800

    wrap in a main()

--- sample_matrixportal.py
+++ sample_matrixportal.py
@@ -28,90 +28,97 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
+def main():  # pylint: disable=too-many-locals
+    """
+    they call it main.
+    """
 
-board_type = os.uname().machine
-if "Matrix Portal M4" not in board_type:
-    print(f"unsupported board type: {board_type}")
-    print("this code is designed to run on MatrixPortal M4")
-    sys.exit(1)
+    board_type = os.uname().machine
+    if "Matrix Portal M4" not in board_type:
+        print(f"unsupported board type: {board_type}")
+        print("this code is designed to run on MatrixPortal M4")
+        sys.exit(1)
 
-# get rid of any pre-existing display
-displayio.release_displays()
+    # get rid of any pre-existing display
+    displayio.release_displays()
 
-# see https://learn.adafruit.com/rgb-led-matrices-matrix-panels-with-circuitpython/advanced-multiple-panels
-# for details on the tile/serpentine/chain values
+    # see https://learn.adafruit.com/rgb-led-matrices-matrix-panels-with-circuitpython/advanced-multiple-panels
+    # for details on the tile/serpentine/chain values
 
-MX_BASE_WIDTH = 64  # width of a single panel
-MX_BASE_HEIGHT = 32  # height of a single panel
-MX_BIT_DEPTH = 6  # valid values: 1-6
-MX_CHAIN_ACROSS = 1  # number of panels across
-MX_TILE_DOWN = 1  # number of panels high
-MX_SERPENTINE = True  # whether alternate panels are rotated to shorten cabling
+    mx_base_width = 64  # width of a single panel
+    mx_base_height = 32  # height of a single panel
+    mx_bit_depth = 6  # valid values: 1-6
+    mx_chain_across = 1  # number of panels across
+    mx_tile_down = 1  # number of panels high
+    mx_serpentine = True  # whether alternate panels are rotated to shorten cabling
 
-MX_WIDTH = MX_BASE_WIDTH * MX_CHAIN_ACROSS
-MX_HEIGHT = MX_BASE_HEIGHT * MX_TILE_DOWN
+    mx_width = mx_base_width * mx_chain_across
+    mx_height = mx_base_height * mx_tile_down
 
-# matrixportal M4 pins
-mx_rgb_pins = [
-    board.MTX_R1,
-    board.MTX_G1,
-    board.MTX_B1,
-    board.MTX_R2,
-    board.MTX_G2,
-    board.MTX_B2,
-]
-mx_addr_pins = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD]
-mx_addr_pins_64 = [
-    board.MTX_ADDRA,
-    board.MTX_ADDRB,
-    board.MTX_ADDRC,
-    board.MTX_ADDRD,
-    board.MTX_ADDRE,
-]
-mx_clock_pin = board.MTX_CLK
-mx_latch_pin = board.MTX_LAT
-mx_output_enable_pin = board.MTX_OE
+    # matrixportal M4 pins
+    mx_rgb_pins = [
+        board.MTX_R1,
+        board.MTX_G1,
+        board.MTX_B1,
+        board.MTX_R2,
+        board.MTX_G2,
+        board.MTX_B2,
+    ]
+    mx_addr_pins = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD]
+    mx_addr_pins_64 = [
+        board.MTX_ADDRA,
+        board.MTX_ADDRB,
+        board.MTX_ADDRC,
+        board.MTX_ADDRD,
+        board.MTX_ADDRE,
+    ]
+    mx_clock_pin = board.MTX_CLK
+    mx_latch_pin = board.MTX_LAT
+    mx_output_enable_pin = board.MTX_OE
 
-# create the matrix
-matrix = rgbmatrix.RGBMatrix(
-    width=MX_WIDTH,
-    height=MX_HEIGHT,
-    bit_depth=MX_BIT_DEPTH,
-    tile=MX_TILE_DOWN,
-    serpentine=MX_SERPENTINE,
-    rgb_pins=mx_rgb_pins,
-    addr_pins=mx_addr_pins_64 if MX_BASE_HEIGHT==64 else mx_addr_pins,
-    clock_pin=mx_clock_pin,
-    latch_pin=mx_latch_pin,
-    output_enable_pin=mx_output_enable_pin,
-)
+    # create the matrix
+    matrix = rgbmatrix.RGBMatrix(
+        width=mx_width,
+        height=mx_height,
+        bit_depth=mx_bit_depth,
+        tile=mx_tile_down,
+        serpentine=mx_serpentine,
+        rgb_pins=mx_rgb_pins,
+        addr_pins=mx_addr_pins_64 if mx_base_height==64 else mx_addr_pins,
+        clock_pin=mx_clock_pin,
+        latch_pin=mx_latch_pin,
+        output_enable_pin=mx_output_enable_pin,
+    )
 
-MX_AUTO_REFRESH = True  # or False if refreshing the display manually
-display = framebufferio.FramebufferDisplay(matrix, auto_refresh=MX_AUTO_REFRESH)
+    mx_auto_refresh = True  # or False if refreshing the display manually
+    display = framebufferio.FramebufferDisplay(matrix, auto_refresh=mx_auto_refresh)
 
-master_group = displayio.Group()
+    master_group = displayio.Group()
 
-display.show(master_group)
+    display.show(master_group)
 
-i2c = board.I2C()  # read for accelerometer and RTC
+    i2c = board.I2C()  # read for accelerometer and RTC
 
-# accelerometer - https://docs.circuitpython.org/projects/lis3dh/en/latest/
-lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19)
-# Set range of accelerometer (can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G).
-lis3dh.range = adafruit_lis3dh.RANGE_2_G
-# then do stuff with lis3dh.acceleration or the shake/tap functions
+    # accelerometer - https://docs.circuitpython.org/projects/lis3dh/en/latest/
+    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19)
+    # Set range of accelerometer (can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G).
+    lis3dh.range = adafruit_lis3dh.RANGE_2_G
+    # then do stuff with lis3dh.acceleration or the shake/tap functions
 
-# RTC - https://learn.adafruit.com/adafruit-ds3231-precision-rtc-breakout/circuitpython
-ds3231 = adafruit_ds3231.DS3231(i2c)
-current_time = ds3231.datetime  # struct_time
+    # RTC - https://learn.adafruit.com/adafruit-ds3231-precision-rtc-breakout/circuitpython
+    ds3231 = adafruit_ds3231.DS3231(i2c)
+    current_time = ds3231.datetime  # struct_time
 
 
-# wifi
-esp32_cs = DigitalInOut(board.ESP_CS)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
-# now do things like wifi.get() and wifi.post()
+    # wifi
+    esp32_cs = DigitalInOut(board.ESP_CS)
+    esp32_ready = DigitalInOut(board.ESP_BUSY)
+    esp32_reset = DigitalInOut(board.ESP_RESET)
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+    status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+    wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)  # pylint: disable=unused-variable
+    # now do things like wifi.get() and wifi.post()
+
+if __name__ == "__main__":
+    main()
--- set_ds3231.py
+++ set_ds3231.py
@@ -13,61 +13,65 @@ from adafruit_esp32spi import adafruit_esp32spi
 from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 import adafruit_ds3231
 
-# Get wifi details and more from a secrets.py file
 try:
-    from secrets import secrets
+    from _secrets import af_secrets as secrets
 except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-print("ESP32 local time")
+def main():  # pylint: disable=too-many-locals
+    """
+    they call it main.
+    """
 
-TIME_API = "http://worldtimeapi.org/api/ip"
+    print("ESP32 local time")
 
-esp32_cs = DigitalInOut(board.ESP_CS)
-esp32_ready = DigitalInOut(board.ESP_BUSY)
-esp32_reset = DigitalInOut(board.ESP_RESET)
-spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
-esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
-"""Use below for Most Boards"""
-status_light = neopixel.NeoPixel(
-    board.NEOPIXEL, 1, brightness=0.2
-)  # Uncomment for Most Boards
-"""Uncomment below for ItsyBitsy M4"""
-# status_light = dotstar.DotStar(board.APA102_SCK, board.APA102_MOSI, 1, brightness=0.2)
-wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
+    TIME_API = "http://worldtimeapi.org/api/ip"
 
-i2c = board.I2C()
-ds3231 = adafruit_ds3231.DS3231(i2c)
-# the_rtc = rtc.RTC()
+    esp32_cs = DigitalInOut(board.ESP_CS)
+    esp32_ready = DigitalInOut(board.ESP_BUSY)
+    esp32_reset = DigitalInOut(board.ESP_RESET)
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+    status_light = neopixel.NeoPixel(
+        board.NEOPIXEL, 1, brightness=0.2
+    )  # Uncomment for Most Boards
+    wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
 
-response = None
-while True:
-    try:
-        print("Fetching json from", TIME_API)
-        response = wifi.get(TIME_API)
-        break
-    except OSError as e:
-        print("Failed to get data, retrying\n", e)
-        continue
+    i2c = board.I2C()
+    ds3231 = adafruit_ds3231.DS3231(i2c)
+    # the_rtc = rtc.RTC()
 
-json = response.json()
-current_time = json["datetime"]
-the_date, the_time = current_time.split("T")
-year, month, mday = [int(x) for x in the_date.split("-")]
-the_time = the_time.split(".")[0]
-hours, minutes, seconds = [int(x) for x in the_time.split(":")]
+    response = None
+    while True:
+        try:
+            print("Fetching json from", TIME_API)
+            response = wifi.get(TIME_API)
+            break
+        except OSError as e:  #pylint: disable=invalid-name
+            print("Failed to get data, retrying\n", e)
+            continue
 
-# We can also fill in these extra nice things
-year_day = json["day_of_year"]
-week_day = json["day_of_week"]
-is_dst = json["dst"]
+    json = response.json()
+    current_time = json["datetime"]
+    the_date, the_time = current_time.split("T")
+    year, month, mday = [int(x) for x in the_date.split("-")]
+    the_time = the_time.split(".")[0]
+    hours, minutes, seconds = [int(x) for x in the_time.split(":")]
 
-now = time.struct_time(
-    (year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst)
-)
-print(now)
-ds3231.datetime = now
-#the_rtc.datetime = now
+    # We can also fill in these extra nice things
+    year_day = json["day_of_year"]
+    week_day = json["day_of_week"]
+    is_dst = json["dst"]
 
-print(time.localtime())
+    now = time.struct_time(
+        (year, month, mday, hours, minutes, seconds, week_day, year_day, is_dst)
+    )
+    print(now)
+    ds3231.datetime = now
+    #the_rtc.datetime = now
+
+    print(time.localtime())
+
+if __name__ == "__main__":
+    main()

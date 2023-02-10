9ce7260c4bed01d58398f73117d9924a5a7cb376
Erik Kennedy <kennedye@users.noreply.github.com>
2023-02-09 23:54:11 -0800

    first whack at refactoring into classes

---
+++ led_panel.py
@@ -0,0 +1,55 @@
+import os
+import board
+import rgbmatrix
+
+class led_panel():
+
+    def __init__(self):
+        # see https://learn.adafruit.com/rgb-led-matrices-matrix-panels-with-circuitpython/advanced-multiple-panels
+        # for details on the tile/serpentine/chain values
+
+        # note: these values are read from settings.toml, only available in CircuitPython 8.0 and later
+        # fmt: off
+        mx_base_width = os.getenv("mx_base_width", 32)  # width of a single panel
+        mx_base_height = os.getenv("mx_base_height", 32)  # height of a single panel
+        mx_bit_depth = os.getenv("mx_bit_depth", 3)  # valid values: 1-6
+        mx_chain_across = os.getenv("mx_chain_across", 1)  # number of panels across
+        mx_tile_down = os.getenv("mx_tile_down", 1)  # number of panels high
+        mx_serpentine = (os.getenv("mx_serpentine") == "True", "True")  # whether alternate panels are rotated to shorten cabling
+        mx_auto_refresh = (os.getenv("mx_auto_refresh") == "True", "True")  # or False if refreshing the display manually
+        # fmt: on
+
+        mx_width = mx_base_width * mx_chain_across
+        mx_height = mx_base_height * mx_tile_down
+
+        # matrixportal M4 pins
+        mx_rgb_pins = [
+            board.MTX_R1,
+            board.MTX_G1,
+            board.MTX_B1,
+            board.MTX_R2,
+            board.MTX_G2,
+            board.MTX_B2,
+        ]
+        # fmt: off
+        mx_addr_pins_32 = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD]
+        mx_addr_pins_64 = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD, board.MTX_ADDRE]
+        # fmt: on
+        mx_clock_pin = board.MTX_CLK
+        mx_latch_pin = board.MTX_LAT
+        mx_output_enable_pin = board.MTX_OE
+
+        # create the matrix
+        self.matrix = rgbmatrix.RGBMatrix(
+            width=mx_width,
+            height=mx_height,
+            bit_depth=mx_bit_depth,
+            tile=mx_tile_down,
+            serpentine=mx_serpentine,
+            rgb_pins=mx_rgb_pins,
+            addr_pins=mx_addr_pins_64 if mx_base_height == 64 else mx_addr_pins_32,
+            clock_pin=mx_clock_pin,
+            latch_pin=mx_latch_pin,
+            output_enable_pin=mx_output_enable_pin,
+        )
+
--- sample_matrixportal.py
+++ sample_matrixportal.py
@@ -1,4 +1,4 @@
-# pylint: disable=line-too-long,import-error,unused-import,too-many-locals,invalid-name,unused-variable
+# pylint: disable=line-too-long,import-error,unused-import,too-many-locals,invalid-name,unused-variable,too-many-statements,invalid-envvar-default
 """
 sample.py
 very basic matrixportal code
@@ -21,6 +21,7 @@ from adafruit_esp32spi import adafruit_esp32spi_wifimanager
 import adafruit_requests as requests
 import adafruit_lis3dh  # accelerometer
 import adafruit_ds3231  # RTC
+from led_panel import led_panel
 
 try:
     from _secrets import af_secrets as secrets
@@ -28,70 +29,37 @@ except ImportError:
     print("WiFi secrets are kept in secrets.py, please add them there!")
     raise
 
-def main():
-    """
-    they call it main.
-    """
 
+def compatibility_check():
+    """
+    basic checks to make sure the board and version are correct
+    """
     board_type = os.uname().machine
     if "Matrix Portal M4" not in board_type:
         print(f"unsupported board type: {board_type}")
         print("this code is designed to run on MatrixPortal M4")
         sys.exit(1)
+    cp_info = sys.implementation
+    if cp_info.version[0] < 8:
+        print(f"unsupported CircuitPython major version: {cp_info.version[0]}")
+        print("this code is designed to run on CircuitPython 8.0 or later")
+        sys.exit(1)
+
+
+def main():
+    """
+    they call it main.
+    """
+
+    # is it safe
+    compatibility_check()
 
     # get rid of any pre-existing display
     displayio.release_displays()
 
-    # see https://learn.adafruit.com/rgb-led-matrices-matrix-panels-with-circuitpython/advanced-multiple-panels
-    # for details on the tile/serpentine/chain values
-
-    mx_base_width = 64  # width of a single panel
-    mx_base_height = 32  # height of a single panel
-    mx_bit_depth = 6  # valid values: 1-6
-    mx_chain_across = 1  # number of panels across
-    mx_tile_down = 1  # number of panels high
-    mx_serpentine = True  # whether alternate panels are rotated to shorten cabling
-
-    mx_width = mx_base_width * mx_chain_across
-    mx_height = mx_base_height * mx_tile_down
-
-    # matrixportal M4 pins
-    mx_rgb_pins = [
-        board.MTX_R1,
-        board.MTX_G1,
-        board.MTX_B1,
-        board.MTX_R2,
-        board.MTX_G2,
-        board.MTX_B2,
-    ]
-    mx_addr_pins = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD]
-    mx_addr_pins_64 = [
-        board.MTX_ADDRA,
-        board.MTX_ADDRB,
-        board.MTX_ADDRC,
-        board.MTX_ADDRD,
-        board.MTX_ADDRE,
-    ]
-    mx_clock_pin = board.MTX_CLK
-    mx_latch_pin = board.MTX_LAT
-    mx_output_enable_pin = board.MTX_OE
-
-    # create the matrix
-    matrix = rgbmatrix.RGBMatrix(
-        width=mx_width,
-        height=mx_height,
-        bit_depth=mx_bit_depth,
-        tile=mx_tile_down,
-        serpentine=mx_serpentine,
-        rgb_pins=mx_rgb_pins,
-        addr_pins=mx_addr_pins_64 if mx_base_height==64 else mx_addr_pins,
-        clock_pin=mx_clock_pin,
-        latch_pin=mx_latch_pin,
-        output_enable_pin=mx_output_enable_pin,
-    )
-
-    mx_auto_refresh = True  # or False if refreshing the display manually
-    display = framebufferio.FramebufferDisplay(matrix, auto_refresh=mx_auto_refresh)
+    matrix = led_panel()
+    mx_auto_refresh = (os.getenv("mx_auto_refresh") == "True", "True")  # or False if refreshing the display manually
+    display = framebufferio.FramebufferDisplay(matrix.matrix, auto_refresh=mx_auto_refresh)
 
     master_group = displayio.Group()
 
@@ -109,7 +77,6 @@ def main():
     ds3231 = adafruit_ds3231.DS3231(i2c)
     current_time = ds3231.datetime  # struct_time
 
-
     # wifi
     esp32_cs = DigitalInOut(board.ESP_CS)
     esp32_ready = DigitalInOut(board.ESP_BUSY)
@@ -119,6 +86,7 @@ def main():
     status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
     wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
     # now do things like wifi.get() and wifi.post()
+
 
 if __name__ == "__main__":
     main()

87716905000826f9272a28488ac387dfa50fa3f5
Erik Kennedy <kennedye@users.noreply.github.com>
2023-03-13 22:22:08 -0700

    first pass at matrix code for matrixportal

---
+++ mp_matrix.py
@@ -0,0 +1,128 @@
+# pylint: disable=line-too-long,import-error,unused-import,too-many-locals,invalid-name,unused-variable,too-many-statements,invalid-envvar-default
+"""
+mp_matrix.py
+matrix code-like effect
+"""
+import time
+import os
+import sys
+import random
+import board
+import busio
+import displayio
+import framebufferio
+import rgbmatrix
+import terminalio
+from rainbowio import colorwheel
+from digitalio import DigitalInOut
+import neopixel
+from adafruit_esp32spi import adafruit_esp32spi
+from adafruit_esp32spi import adafruit_esp32spi_wifimanager
+import adafruit_requests as requests
+import adafruit_lis3dh  # accelerometer
+import adafruit_ds3231  # RTC
+from led_panel import LedPanel
+
+try:
+    from _secrets import af_secrets as secrets
+except ImportError:
+    print("WiFi secrets are kept in secrets.py, please add them there!")
+    raise
+
+
+def compatibility_check():
+    """
+    basic checks to make sure the board and version are correct
+    """
+    board_type = os.uname().machine
+    if "Matrix Portal M4" not in board_type:
+        print(f"unsupported board type: {board_type}")
+        print("this code is designed to run on MatrixPortal M4")
+        sys.exit(1)
+    cp_info = sys.implementation
+    if cp_info.version[0] < 8:
+        print(f"unsupported CircuitPython major version: {cp_info.version[0]}")
+        print("this code is designed to run on CircuitPython 8.0 or later")
+        sys.exit(1)
+
+
+def code_line():
+    """
+    generate a vertical line of pixels
+    """
+    palette1 = displayio.Palette(8)
+    pos = 0
+    for i in range(16,128,16):
+        palette1[pos] = (0, i, 0)
+        pos += 1
+    palette1[7] = (255, 255, 255)
+    bitmap1 = displayio.Bitmap(1, 8, 8)
+    pos = 0
+    for i in range(8):
+        bitmap1[0, pos] = pos
+        pos += 1
+    
+    return displayio.TileGrid(bitmap1, pixel_shader=palette1)
+
+def main():
+    """
+    they call it main.
+    """
+
+    # is it safe
+    compatibility_check()
+
+    # get rid of any pre-existing display
+    displayio.release_displays()
+
+    panel = LedPanel()
+    mx_auto_refresh = (os.getenv("mx_auto_refresh") == "True", "True")  # or False if refreshing the display manually
+    display = framebufferio.FramebufferDisplay(panel.matrix, auto_refresh=mx_auto_refresh)
+
+    master_group = displayio.Group()
+
+    display.show(master_group)
+
+    i2c = board.I2C()  # read for accelerometer and RTC
+
+    # accelerometer - https://docs.circuitpython.org/projects/lis3dh/en/latest/
+    lis3dh = adafruit_lis3dh.LIS3DH_I2C(i2c, address=0x19)
+    # Set range of accelerometer (can be RANGE_2_G, RANGE_4_G, RANGE_8_G or RANGE_16_G).
+    lis3dh.range = adafruit_lis3dh.RANGE_2_G
+    # then do stuff with lis3dh.acceleration or the shake/tap functions
+
+    # RTC - https://learn.adafruit.com/adafruit-ds3231-precision-rtc-breakout/circuitpython
+    ds3231 = adafruit_ds3231.DS3231(i2c)
+    current_time = ds3231.datetime  # struct_time
+
+    # wifi
+    esp32_cs = DigitalInOut(board.ESP_CS)
+    esp32_ready = DigitalInOut(board.ESP_BUSY)
+    esp32_reset = DigitalInOut(board.ESP_RESET)
+    spi = busio.SPI(board.SCK, board.MOSI, board.MISO)
+    esp = adafruit_esp32spi.ESP_SPIcontrol(spi, esp32_cs, esp32_ready, esp32_reset)
+    status_light = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.2)
+    wifi = adafruit_esp32spi_wifimanager.ESPSPI_WiFiManager(esp, secrets, status_light)
+    # now do things like wifi.get() and wifi.post()
+
+    while True:
+        group1 = displayio.Group()
+        
+        tile1 = code_line()
+        group1.append(tile1)
+
+        group1.x = random.randint(0, panel.matrix.width)
+        group1.y = -8
+
+        master_group.append(group1)
+        display.show(master_group)
+
+        time.sleep(2)
+
+        pos = group1.y
+        for i in range(pos, panel.matrix.height + 8):
+            time.sleep(0.05)
+            group1.y = i
+
+if __name__ == "__main__":
+    main()

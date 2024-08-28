f584a2a702270ca8665dc40c7d227394f8d7b88b
Erik Kennedy <kennedye@users.noreply.github.com>
2024-08-27 21:48:52 -0700

    a better way of handling fifth pin

--- led_panel.py
+++ led_panel.py
@@ -1,11 +1,11 @@
-# pylint: disable=line-too-long,import-error,unused-import,too-many-locals,invalid-name,unused-variable,too-many-statements,invalid-envvar-default,too-few-public-methods
+# pylint: disable=import-error,unused-import
 import os
 import board
 import rgbmatrix
 
-class LedPanel():
-    """simple class to define a panel instance
-    """
+
+class LedPanel:
+    """simple class to define a panel instance"""
 
     def __init__(self):
         # see https://learn.adafruit.com/rgb-led-matrices-matrix-panels-with-circuitpython/advanced-multiple-panels
@@ -13,11 +13,11 @@ class LedPanel():
 
         # note: these values are read from settings.toml, only available in CircuitPython 8.0 and later
         # fmt: off
-        panel_base_width = os.getenv("mx_base_width", 32)  # width of a single panel
-        panel_base_height = os.getenv("mx_base_height", 32)  # height of a single panel
-        panel_bit_depth = os.getenv("mx_bit_depth", 3)  # valid values: 1-6
-        panel_chain_across = os.getenv("mx_chain_across", 1)  # number of panels across
-        panel_tile_down = os.getenv("mx_tile_down", 1)  # number of panels high
+        panel_base_width = os.getenv("mx_base_width", "32")  # width of a single panel
+        panel_base_height = os.getenv("mx_base_height", "32")  # height of a single panel
+        panel_bit_depth = os.getenv("mx_bit_depth", "3")  # valid values: 1-6
+        panel_chain_across = os.getenv("mx_chain_across", "1")  # number of panels across
+        panel_tile_down = os.getenv("mx_tile_down", "1")  # number of panels high
         panel_serpentine = (os.getenv("mx_serpentine") == "True", "True")  # whether alternate panels are rotated to shorten cabling
         # fmt: on
 
@@ -25,9 +25,10 @@ class LedPanel():
         panel_height = panel_base_height * panel_tile_down
 
         # what kind of board are we
-        board_type = os.uname().machine
-        if "Matrix" in board_type:
-            # matrixportal M4/S3 pins
+        if (
+            "MatrixPortal S3" in os.uname().machine
+            or "Matrix Portal M4" in os.uname().machine
+        ):  # Adafruit Matrix Portal M4/S3
             panel_rgb_pins = [
                 board.MTX_R1,
                 board.MTX_G1,
@@ -36,31 +37,49 @@ class LedPanel():
                 board.MTX_G2,
                 board.MTX_B2,
             ]
-            # fmt: off
-            panel_addr_pins_32 = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD]
-            panel_addr_pins_64 = [board.MTX_ADDRA, board.MTX_ADDRB, board.MTX_ADDRC, board.MTX_ADDRD, board.MTX_ADDRE]
-            # fmt: on
+            panel_addr_pins = [
+                board.MTX_ADDRA,
+                board.MTX_ADDRB,
+                board.MTX_ADDRC,
+                board.MTX_ADDRD,
+            ]
+            if panel_base_height > 32:
+                panel_addr_pins.append(board.MTX_ADDRE)
             panel_clock_pin = board.MTX_CLK
             panel_latch_pin = board.MTX_LAT
             panel_output_enable_pin = board.MTX_OE
 
-        elif "Pimoroni Interstate 75" in board_type:
-            # interstate75 pins
+        elif "Pimoroni Interstate 75" in os.uname().machine:  # regular Interstate 75
             panel_rgb_pins = [
                 board.R0,
                 board.G0,
                 board.B0,
                 board.R1,
                 board.G1,
-                board.B1
+                board.B1,
             ]
-            # fmt: off
-            panel_addr_pins_32 = [board.ROW_A, board.ROW_B, board.ROW_C, board.ROW_D]
-            panel_addr_pins_64 = [board.ROW_A, board.ROW_B, board.ROW_C, board.ROW_D, board.ROW_E]
-            # fmt: on
+            panel_addr_pins = [board.ROW_A, board.ROW_B, board.ROW_C, board.ROW_D]
+            if panel_base_height > 32:
+                panel_addr_pins.append(board.ROW_E)
             panel_clock_pin = board.CLK
             panel_latch_pin = board.LAT
             panel_output_enable_pin = board.OE
+
+        elif "Raspberry Pi Pico W with rp2040" in os.uname().machine:  # Interstate 75 W
+            panel_rgb_pins = [
+                board.GP0,
+                board.GP1,
+                board.GP2,
+                board.GP3,
+                board.GP4,
+                board.GP5,
+            ]
+            panel_addr_pins = [board.GP6, board.GP7, board.GP8, board.GP9]
+            if panel_base_height > 32:
+                panel_addr_pins.append(board.GP10)
+            panel_clock_pin = board.GP11
+            panel_latch_pin = board.GP12
+            panel_output_enable_pin = board.GP13
 
         # create the matrix
         self.matrix = rgbmatrix.RGBMatrix(
@@ -70,7 +89,7 @@ class LedPanel():
             tile=panel_tile_down,
             serpentine=panel_serpentine,
             rgb_pins=panel_rgb_pins,
-            addr_pins=panel_addr_pins_64 if panel_base_height == 64 else panel_addr_pins_32,
+            addr_pins=panel_addr_pins,
             clock_pin=panel_clock_pin,
             latch_pin=panel_latch_pin,
             output_enable_pin=panel_output_enable_pin,
